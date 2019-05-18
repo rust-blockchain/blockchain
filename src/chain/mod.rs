2ed6ee2591b97d07dca3fd635919db9451025813
Wei Tang <hi@that.world>
2019-05-18 02:16:27 +0200

    Add helper struct SharedImportBlock

--- src/chain/mod.rs
+++ src/chain/mod.rs
@@ -6,6 +6,8 @@ mod block_builder;
 pub use self::importer::{SharedBackend, Importer};
 pub use self::block_builder::BlockBuilder;
 
+use crate::traits::ImportBlock;
+use std::sync::{Arc, Mutex};
 use std::{fmt, error as stderror};
 
 /// Error type for chain.
@@ -34,5 +36,37 @@ impl stderror::Error for Error {
 			Error::Executor(e) => Some(e.as_ref()),
 			Error::IsGenesis | Error::ParentNotFound => None,
 		}
+	}
+}
+
+/// An import block that can be shared across threads.
+pub struct SharedImportBlock<I: ImportBlock> {
+	import_block: Arc<Mutex<I>>,
+}
+
+impl<I: ImportBlock> SharedImportBlock<I> {
+	/// Create a new shared import block.
+	pub fn new(import_block: I) -> Self {
+		Self {
+			import_block: Arc::new(Mutex::new(import_block)),
+		}
+	}
+}
+
+impl<I: ImportBlock> Clone for SharedImportBlock<I> {
+	fn clone(&self) -> Self {
+		Self {
+			import_block: self.import_block.clone(),
+		}
+	}
+}
+
+impl<I: ImportBlock> ImportBlock for SharedImportBlock<I> {
+	type Block = I::Block;
+	type Error = I::Error;
+
+	fn import_block(&mut self, block: Self::Block) -> Result<(), Self::Error> {
+		self.import_block.lock().expect("Lock is poisoned")
+			.import_block(block)
 	}
 }
