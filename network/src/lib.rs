cbe7710a859f47655a486ea9521c5fd924998859
Wei Tang <hi@that.world>
2019-09-08 22:57:01 +0200

    [WIP] Remove unused network environment

--- network/src/lib.rs
+++ network/src/lib.rs
@@ -1,19 +1,1 @@
 pub mod sync;
-
-pub trait NetworkEnvironment {
-	type PeerId;
-	type Message;
-}
-
-pub trait NetworkHandle: NetworkEnvironment {
-	fn send(&mut self, peer: &Self::PeerId, message: Self::Message);
-	fn broadcast(&mut self, message: Self::Message);
-}
-
-pub trait NetworkEvent: NetworkEnvironment {
-	fn on_tick<H: NetworkHandle>(&mut self, _handle: &mut H) where
-		H: NetworkEnvironment<PeerId=Self::PeerId, Message=Self::Message> { }
-	fn on_message<H: NetworkHandle>(
-		&mut self, _handle: &mut H, _peer: &Self::PeerId, _message: Self::Message
-	) where H: NetworkEnvironment<PeerId=Self::PeerId, Message=Self::Message> { }
-}
